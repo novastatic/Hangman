3782e5e93182c2a2be02e12513a3a27abfec029f
Maximilian97 <maximilian-hedderich@web.de>
2021-04-19 10:06:53 +0200

    First Commit

--- main.c
+++ main.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+void userInput();
+int checkGivenGuesses(char input);
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("Welcome to Hangman\n");
+    userInput();
+
+    return 0;
+}
+
+void userInput()
+{
+    char input;
+    int check;
+
+    do
+    {
+        printf("Please input your guess: ");
+        scanf("%c", &input);
+
+        if(checkGivenGuesses(input) == 0)
+        {
+            printf("You already guessed this letter!");
+        }
+        else
+        {
+            check++;
+        }
+        fflush(stdin);
+    }
+    while(check == 0);
+}
+
+int checkGivenGuesses(char input)
+{
+    char alphabet[28] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ü', 'ö'};
+
+    input = tolower(input);
+    for(int i = 0; i < strlen(alphabet); i++)
+    {
+        if(input == alphabet[i])
+        {
+            alphabet[i] = toupper(alphabet[i]);
+            return 1;
+        }
+    }
     return 0;
 }
